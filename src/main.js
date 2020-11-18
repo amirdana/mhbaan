4f0cac77fef062f0f21e72d0e1c670cfef84a7ec
amirdana <lavasaniamirhossein@gmail.com>
2020-11-18 11:41:24 +0330

    add bootstrap.min.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
 import VueMq from "vue-mq";
+import "./styles/bootstrap.min.css";
 
 Vue.use(VueMq, {
   defaultBreakpoint: "default",
