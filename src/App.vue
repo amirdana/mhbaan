c729bc49991398968395355ab46ff13ad1bb56e5
amirdana <lavasaniamirhossein@gmail.com>
2020-11-20 19:26:48 +0330

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
+  <div class="body-container">
+    <top-nav-mobile v-if="$mq === 'sm' || $mq === 'md'" />
+    <top-nav v-else />
     <transition name="fade" mode="out-in">
-      <router-view style="animation-duration: 0.2s" />
+      <router-view class="nav-offset" style="animation-duration: 0.2s" />
     </transition>
+    <bottom-nav v-if="$mq === 'sm' || $mq === 'md'" />
   </div>
 </template>
 
 <script>
+import TopNav from "@/components/topNavigator";
+import BottomNav from "@/components/bottomNavigator";
+import TopNavMobile from "@/components/topNavigatorMobile";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    TopNav,
+    BottomNav,
+    TopNavMobile,
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+.nav-offset {
+  padding-top: $navbarHeight;
+  padding-bottom: $bottomBarHeight + 30px;
 }
 </style>
